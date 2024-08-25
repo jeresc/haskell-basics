e21a5f47662cc500e92d90a81419020cdc83b6ff
jeresc <jeremiastomassrc@gmail.com>
2024-08-25 19:35:25 -0300

    feat: add basic types of haskell example

---
+++ main.hs
@@ -0,0 +1,46 @@
+
+import Data.List
+import System.IO
+
+-- Int -2^63 2^63
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+-- Integer
+-- Float
+
+-- Doubles 11 points of precision
+bigFloat = 3.99
+
+-- Bool True False
+-- Char
+-- Tuple
+
+always5 :: Int
+always5 = 5
+
+sumOfNums = sum [1..1000]
+
+-- * / - +
+addEx = 5 + 4 
+
+modEx = mod 5 4
+modEx2 = 5 `mod` 4
+
+negNumEx = 5 + (-4)
+
+num9 = 9
+sqrtOf9 = sqrt (fromIntegral num9)
+
+-- Built in math functions
+piVal = pi
+ePow9 = exp 9
+logOf9 = log 9
+squared9 = 9 ** 2
+truncatedVal = truncate 9.999
+roundVal = round 9.999
+ceilingVal = ceiling 9.999
+floorVar = floor 9.999
+
+-- Also sin, cos, tan, asin, atan, acos, sinh,
+-- tanh, cosh, asinh, atanh, acosh
