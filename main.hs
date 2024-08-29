f95a453a2e9e5aead5812299edc0000a60f4949c
jeresc <jeremiastomassrc@gmail.com>
2024-08-28 22:45:35 -0300

    feat: add more list manipulation examples

--- main.hs
+++ main.hs
@@ -2,6 +2,8 @@
 import Data.List
 import System.IO
 
+primes = [3,13,17,19,23,29,31]
+
 infinPow10 = [10,20..]
 
 many2s = take 10 (repeat 2)
@@ -16,3 +18,13 @@ listTimes3 = [x * 3 | x <- [1..10], x * 3 <= 10]
 dividesBy9n13 = [ x | x <- [1..500], x `mod` 13 == 0, x `mod` 9 == 0]
 
 sortedList = sort [3,1,9,5,6]
+
+sumOfLists = zipWith (+) listTimes2 listTimes3
+
+listBiggerThan5 = filter (>5) primes 
+
+evensUpTo20 = takeWhile (<20) [2,4..]
+
+-- also foldr
+multOfList = foldl (*) 1 [2,3,4,5]
+
